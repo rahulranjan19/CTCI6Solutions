1ed4253824082d6e150d3240fbd69cb8a6f28ec1
Rahul Ranjan <rahul_ranjan19@yahoo.com>
2016-11-27 12:31:18 +0530

    Added Chap1 A1

--- solutions/src/main/java/ctci/solutions/com/App.java
+++ solutions/src/main/java/ctci/solutions/com/App.java
@@ -1,13 +1,14 @@
 package ctci.solutions.com;
 
+import ctci.solutions.com.chap1.arrays.Q1IsUnique;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("calling chap1 solns");
+		Q1IsUnique.runSolution();
+	}
 }
--- solutions/src/main/java/ctci/solutions/com/chap1/arrays/Q1IsUnique.java
+++ solutions/src/main/java/ctci/solutions/com/chap1/arrays/Q1IsUnique.java
@@ -1,5 +1,47 @@
 package ctci.solutions.com.chap1.arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Q1IsUnique {
 
+	private static final String correct = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String wrong = "ABCDEFGHIJKKLMNOPQRSTUVWXYZ";
+
+	public static void runSolution() {
+		System.out.println("Checking for unique chars in a string.");
+		System.out.println(" With correct string: " + Q1IsUnique.hasUniqueCharDS(correct));
+		System.out.println(" With incorrect string: " + Q1IsUnique.hasUniqueCharDS(wrong));
+		System.out.println(" With correct string: " + Q1IsUnique.hasUniqueChar("correct"));
+		System.out.println(" With incorrect string: " + Q1IsUnique.hasUniqueChar("wrong"));
+	}
+
+	private static boolean hasUniqueCharDS(String string) {
+		boolean hasUniqueChar = true;
+		Set<Character> uniqueChar = new HashSet<>();
+
+		for (Character charArr : string.toCharArray()) {
+			if (!uniqueChar.add(charArr)) {
+				hasUniqueChar = false;
+				break;
+			}
+		}
+
+		return hasUniqueChar;
+	}
+
+	private static boolean hasUniqueChar(String string) {
+		boolean hasUniqueChar = true;
+		char[] charArr = new char[256];
+
+		for (char temp : string.toCharArray()) {
+			if (charArr[temp] != 0) {
+				hasUniqueChar = false;
+				break;
+			}
+			charArr[temp] = 1;
+		}
+
+		return hasUniqueChar;
+	}
 }
